9520e4ee4764f36746529e1c710a7f35c4e5fd23
David Friend <david@ameronix.com>
2018-04-01 19:41:54 -0400

    Changed animations of tiles and spacing

--- src/components/GameTile/GameTile.js
+++ src/components/GameTile/GameTile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './GameTile.css';
 
-const GameTile = props => <img className="gameTile" src={props.link} alt='dog' onClick={() => props.onClicked(props.id)} />;
+const GameTile = props => <img className="gameTile hvr-wobble-top" src={props.link} alt='dog' onClick={() => props.onClicked(props.id)} />;
 
 export default GameTile;
